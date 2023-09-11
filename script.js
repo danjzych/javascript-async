54453ee9f2e21c3aacd23554dc9c30e1736c3fd4
Daniel Zych <danjzych@gmail.com>
2023-09-11 18:39:48 -0500

    Solved problem number 3

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ function showNumberRace(num1, num2, num3, num4) {
     );
   });
 }
+
+
+async function showNumberAll(num1, num2, num3, num4) {
+  console.debug('showNumberAll');
+
+  const num1trivia = fetch(`${BASE_URL}/${num1}?json`);
+  const num2trivia = fetch(`${BASE_URL}/${num2}?json`);
+  const num3trivia = fetch(`${BASE_URL}/${num3}?json`);
+  const num4trivia = fetch(`${BASE_URL}/${num4}?json`);
+
+  const results = await Promise.allSettled([num1trivia, num2trivia, num3trivia, num4trivia]);
+
+  const okPromises = results
+    .filter(promise => promise.status === 'fulfilled' && promise.value.ok)
+    .map(promise => promise.value.json());
+
+  const okJSON = await Promise.all(okPromises);
+  const okText = okJSON.map(json => json.text);
+  console.log(okText);
+
+  const notOkPromises = results
+    .filter(promise => !promise.value.ok)
+    .map(promise => promise.value.statusText);
+
+    console.log(notOkPromises);
+}
