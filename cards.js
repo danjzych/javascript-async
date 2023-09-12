cf5fd95834db470f747ed2368426956c639e7d25
Christopher Alley <calleyviola@gmail.com>
2023-09-11 20:53:12 -0400

    Add event handler function

--- cards.js
+++ cards.js
@@ -1,10 +1,12 @@
-"use strict"
+"use strict";
 
 let deckId;
 
-const BASE_URL = 'https://deckofcardsapi.com/api/deck'
-const GET_CARD_BUTTON = document.querySelector('#get-card-form');
+const BASE_URL = 'https://deckofcardsapi.com/api/deck';
+const $cardForm = $('#get-card-form');
+const $cardContainer = $('#card-container');
 
+/** Get deck-id from API */
 async function getNewDeck() {
   console.debug('getNewDeck');
 
@@ -14,16 +16,29 @@ async function getNewDeck() {
   deckId = data.deck_id;
 }
 
+/** Get a card from shuffled deck from API */
 async function drawCard() {
   console.debug('drawCard');
 
   const resp = await fetch(`${BASE_URL}/${deckId}/draw`);
   const data = await resp.json();
 
-  return data.cards[0].image;
+  return { image: data.cards[0].image, remaining: data.remaining };
 }
 
+/** Append card to DOM and hide button after deck is empty */
+async function handleCardClick(evt) {
+  evt.preventDefault();
 
-GET_CARD_BUTTON.addEventListener('submit', )
+  const $card = await drawCard();
+  const $newCard = $(`<img src="${$card.image}">`);
+  $cardContainer.prepend($newCard);
 
-await getNewDeck();
+  if ($card.remaining === 0) {
+    $cardForm.hide();
+  }
+}
+
+$cardForm.on('submit', handleCardClick);
+
+getNewDeck();
