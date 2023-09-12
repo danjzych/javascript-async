97f9f09740c71872cb23d1e80e9ad772af49b50a
Christopher Alley <calleyviola@gmail.com>
2023-09-11 20:05:25 -0400

    Add main and refactor race

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
+"use strict";
+
 const BASE_URL = 'http://numbersapi.com';
+
 
 async function showNumberTrivia(num) {
   const resp = await fetch(`${BASE_URL}/${num}?json`);
 
   const data = await resp.json();
 
-  console.log(data.text);
+  console.log("showNumberTrivia: ", data.text);
 }
 
-function showNumberRace(num1, num2, num3, num4) {
+
+async function showNumberRace(num1, num2, num3, num4) {
   const num1trivia = fetch(`${BASE_URL}/${num1}?json`);
   const num2trivia = fetch(`${BASE_URL}/${num2}?json`);
   const num3trivia = fetch(`${BASE_URL}/${num3}?json`);
   const num4trivia = fetch(`${BASE_URL}/${num4}?json`);
 
-  const answerPromise = Promise.race([num1trivia, num2trivia, num3trivia, num4trivia]);
+  const answerPromise = await Promise.race([num1trivia, num2trivia, num3trivia, num4trivia]);
+  const raceWinner = await answerPromise.json()
 
-  answerPromise.then(function logWinner(winner) {
-    winner.json().then(winningResponse => {
-      console.log("winningResponse=", winningResponse.text);
-    }
-    );
-  });
+  console.log("showNumberRace: ", raceWinner.text);
 }
 
 
-async function showNumberAll(num1, num2, num3, num4) {
-  console.debug('showNumberAll');
+async function showNumberAll(num1, num2, num3, num4="WRONG") {
+  // console.debug('showNumberAll');
 
   const num1trivia = fetch(`${BASE_URL}/${num1}?json`);
   const num2trivia = fetch(`${BASE_URL}/${num2}?json`);
@@ -41,11 +41,18 @@ async function showNumberAll(num1, num2, num3, num4) {
 
   const okJSON = await Promise.all(okPromises);
   const okText = okJSON.map(json => json.text);
-  console.log(okText);
+  console.log("showNumberAllFulfilled: ", okText);
 
   const notOkPromises = results
     .filter(promise => !promise.value.ok)
-    .map(promise => promise.value.statusText);
+    .map(promise => `Request failed with status code ${promise.value.status}`);
 
-    console.log(notOkPromises);
+    console.log("showNumberAllRejected: ", notOkPromises);
+}
+
+
+async function main(num1, num2, num3, num4) {
+  await showNumberTrivia(num1);
+  await showNumberRace(num1, num2, num3, num4);
+  await showNumberAll(num1, num2, num3);
 }
